7d5d788aa1052f7f183b8d37753ac9d0279ee6c5
Natalia49340 <129343279+Natalia49340@users.noreply.github.com>
2023-06-16 19:21:34 +0200

    Zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .yml

--- save_yaml.py
+++ save_yaml.py
@@ -1,38 +1,32 @@
+import sys
 import yaml
 
 def file_syntax(yaml_str):
-    try:
-        yaml.safe_load(yaml_str)
-        return True
-    except yaml.YAMLError:
-        return False
+     try:
+         yaml.safe_load(yaml_str)
+         return True
+     except yaml.YAMLError:
+         return False
 
- #wczytywanie danych z pliku YAML
+# wczytywanie danych z pliku YAML
 def load_yaml_data(file_path):
-    with open(file_path, "r") as file:
-        yaml_content = file.read()
-        if not file_syntax(yaml_content):
-            print("Niepoprawna składnia pliku YAML:", file_path)
-            return None
-        try:
-            data = yaml.safe_load(yaml_content)
-            return data
-        except FileNotFoundError:
-            print("Podany plik nie istnieje")
-        except yaml.YAMLError as e:
-            print("Błąd wczytywania danych YAML:", file_path)
-        except Exception as e:
-            print("Wystąpił nieoczekiwany błąd:", file_path)
-        return None
+     with open(file_path, "r") as file:
+         yaml_content = file.read()
+         if not file_syntax(yaml_content):
+             print("Niepoprawna składnia pliku YAML:", file_path)
+             return None
+         try:
+             data = yaml.safe_load(yaml_content)
+             return data
+         except FileNotFoundError:
+             print("Podany plik nie istnieje")
+         except yaml.YAMLError as e:
+             print("Błąd wczytywania danych YAML:", file_path)
+         except Exception as e:
+             print("Wystąpił nieoczekiwany błąd:", file_path)
+         return None
 
-input_file = input("Podaj nazwę pliku: ")
-data = load_yaml_data(input_file)
-
-if data:
-    print("Wczytano dane z pliku YAML:")
-    print(data)
-
- #zapis danych do pliku YAML
+#zapis danych do pliku YAML
 def save_yaml_data(data,file_path):
     with open(file_path,"w") as file:
         try:
@@ -41,5 +35,18 @@ def save_yaml_data(data,file_path):
         except Exception as e:
             print("Wystąpił błąd podczas zapisu danych YAML:", file_path)
 
-output_file = input("Podaj nazwę pliku do zapisu: ")
-save_yaml_data(data,output_file)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Podaj nazwy plików wejściowego i wyjściowego jako argumenty.")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    data = load_yaml_data(input_file)
+
+    if data:
+        print("Wczytano dane z pliku YAML:")
+        print(data)
+        save_yaml_data(data, output_file)
+        print(f"Pomyślnie zapisano dane JSON do pliku {output_file}")
